eb768b85e66c34dc21abbc3ab030d121727fab2a
Evgeny Pesterev <apsterio.anima@gmail.com>
2022-04-16 21:32:28 +0500

    Add icons, add navigation, add custom button, add custom textField

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by evpes on 16.04.2022.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,21 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by evpes on 16.04.2022.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+
+
+}
